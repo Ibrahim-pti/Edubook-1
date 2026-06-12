3dbc08644c6ba28bc3980dc71f009a387822bd9a
ibrahim-dev11 <ibrahim.ahmed@pti.edu.krd>
2026-06-12 22:02:53 +0300

    chore: update favicon asset path in Filament admin panel and welcome view

--- xwendngakan-backend/app/Providers/Filament/AdminPanelProvider.php
+++ xwendngakan-backend/app/Providers/Filament/AdminPanelProvider.php
@@ -33,7 +33,7 @@ class AdminPanelProvider extends PanelProvider
             ->login(CustomLogin::class)
             ->brandName('edu book')
             ->brandLogo(fn () => view('filament.components.logo'))
-            ->favicon(asset('favicon.ico'))
+            ->favicon(asset('images/app_logo.png'))
             ->colors([
                 'primary' => Color::hex('#c49a3c'),
                 'danger'  => Color::Rose,
--- xwendngakan-backend/resources/views/welcome.blade.php
+++ xwendngakan-backend/resources/views/welcome.blade.php
@@ -4,7 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>edu book - پلاتفۆرمی پەروەردەیی مۆدێرن</title>
-
+    <link rel="icon" href="{{ asset('images/app_logo.png') }}" type="image/png">
+    <link rel="apple-touch-icon" href="{{ asset('images/app_logo.png') }}">
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
